876a19b3dee4fc78170487a326eeec45e1f54111
Asim Himani <ahimani@sfu.ca>
2017-04-18 23:34:03 -0700

    Updated Sort

--- priorityqueue_sort.cpp
+++ priorityqueue_sort.cpp
@@ -1,28 +1,4 @@
-// a4.cpp
-
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
-// Name : Asim Himani
-// St.# : 301276136
-// Email: ahimani@sfu.ca
-//
-//
-// Statement of Originality
-// ------------------------
-//
-// All the code and comments below are my own original work. For any non-
-// original work, I have provided citations in the comments with enough
-// detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, I have not shared this work with anyone else, and I have
-// not seen solutions from other students, tutors, websites, books,
-// etc.
-//
-/////////////////////////////////////////////////////////////////////////
+// priorityqueue_sort.cpp
 
 #include <algorithm>    // std::sort
 #include <iostream>     // std::cout
